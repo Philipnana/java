4f2846d9728ee9ec9d3911e0933979cfee0ee647
philipnana <philipnana>
2022-04-14 14:53:36 +0000

    assignment

---
+++ assigment.js
@@ -0,0 +1,17 @@
+// names
+var fName = "nana";
+var lName = "asare";
+
+var fName = fName.slice(0,4);
+var lName = lName.slice(0,1);
+
+console.log(fName + lName);
+
+
+// accept two numbers
+const num1 = 5;
+const num2 = 5;
+// add two numbers
+const sum = num1 + num2;
+//display the sum
+console.log("the sum of " + num1 + " and " + num2 + " is " + sum);
